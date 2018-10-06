ca1bbabb8e69294133c507352c4271509905a19f
jspadafora <james@jvspad.com>
2018-10-06 08:58:57 -0700

    More Dev
    Signed-off-by: jspadafora <james@jvspad.com>

---
+++ openPipeHooks/maya_checkin_hooks.py
@@ -0,0 +1,8 @@
+# Copyright 2018 - James Spadafora
+# Open Pipe
+# Hooks for Maya
+
+
+def process_maya_checkin(n, a):
+    """Open the provided maya file and edit the connections in the reconnect list."""
+    print("hello world")
